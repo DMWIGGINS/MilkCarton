462475a8dc05d4abc72231926bac05d3d2f465e5
Katie DeAngelis <katie.e.deangelis@gmail.com>
2018-03-11 21:58:45 -0400

    Finished majority of styling for search page. Still working on responsiveness. There's an interesting issue with flexbox that I need to think about. It should scroll, but instead, it's squishing the search box and carousel. Will revisit.

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,40 +1,72 @@
 // import "./carousel.css";
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Heading from "../Heading";
 import {Carousel} from 'react-materialize';
 import GoogleMap from "../GoogleMap";
 
-
-
-class SearchCarousel extends Component{
+class SearchCarousel extends Component {
     getCarousel() {
-        let content = this.props.searchResults.map(function(result) {
-            return (
-            <div key={result.caseNumber}>
-                <div className="card">
-                    <div className="card-image">
-                        <img src="https://images.askmen.com/1080x540/2017/01/04-123847-what_stoicism_teaches_us_about_how_to_be_a_man.jpg" alt="" />
-                        <span className="card-title">{result.firstName + " " + result.lastName}</span>
-                        <span>{result.state}</span>
+        let content = this
+            .props
+            .searchResults
+            .map(function (result) {
+                return (
+                    <div key={result.caseNumber}>
+                        <div className="card">
+                            <div className="card-content left-align">
+                                <Heading level={5}>
+                                    <b>{result.firstName + " " + result.lastName}</b>
+                                </Heading>
+                                <a className="btn-floating halfway-fab waves-effect waves-light red darken-4">
+                                    <i className="material-icons">remove_red_eye</i>
+                                </a>
+                            </div>
+                            <div className="card-image left-align">
+                                <img src={result.Images[0].photo} alt=""/>
+                                <div className="card-header-data">
+                                    <div>
+                                        <b>Sex: </b>
+                                        {result.sex}</div>
+                                    <div>
+                                        <b>Hair color: </b>
+                                        {result.hairColor}</div>
+                                    <div>
+                                        <b>Eye color: </b>
+                                        {result.leftEyeColor + "/" + result.rightEyeColor}</div>
+                                    <div>
+                                        <b>Current age: </b>
+                                        {result.ageNow}</div>
+                                    <div>
+                                        <b>Height: </b> 
+                                        {result.height}</div>
+                                </div>
+                            </div>
+                            <div className="card-sightings">
+                                <div className="or-seperator">
+                                    <div className="line"></div>
+                                    <div className="or-text">Sightings</div>
+                                    <div className="line"></div>
+                                </div>
+                                <GoogleMap locations={result.Sightings}/>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-content">
-                        <p>{result.circumstances}</p>
-                        <GoogleMap locations={result.Sightings}/>
-                    </div>
-                </div>
-            </div>)
-        })
+                )
+            })
         if (content.length > 0) {
-            return (<Carousel>
-                {content}
-            </Carousel>)
+            return (
+                <Carousel options={this.props.options}>
+                    {content}
+                </Carousel>
+            )
         } else {
-            return (<div>empty</div>)
+            return (
+                <div></div>
+            )
         }
 
-     
-
     }
-    render(){
+    render() {
         return this.getCarousel()
     }
 }
