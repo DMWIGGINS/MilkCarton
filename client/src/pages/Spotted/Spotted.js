b31da4896de0cea3f8fe476caa950f6936e542bb
Helen Pitt <helenpitt1990@gmail.com>
2018-03-12 23:24:26 -0400

    updated spotted page

--- client/src/pages/Spotted/Spotted.js
+++ client/src/pages/Spotted/Spotted.js
@@ -2,7 +2,7 @@ import "../common.css";
 import "./spotted.css";
 import React, { Component } from 'react';
 import Heading from "../../components/Heading";
-import {Row, Col, Autocomplete} from 'react-materialize';
+import {Row, Col, Autocomplete, Input, CardPanel, Button} from 'react-materialize';
 
 class Spotted extends Component {
     render() {
@@ -10,26 +10,25 @@ class Spotted extends Component {
             <Row className="body-background-gradient">
                 <Col s={12} m={5} className="left-gradient">
                 <div className="left-gradient-content">
-                <Heading className="heading" level={2}>Spotted</Heading>
-                    <div className="card">
-                        <div className="card-image">
-                            <img src="https://images.askmen.com/1080x540/2017/01/04-123847-what_stoicism_teaches_us_about_how_to_be_a_man.jpg" alt="" />
-                            {/* <span className="card-title">{result.firstName + " " + result.lastName}</span>
-                            <span>{result.state}</span> */}
-                        </div>
-                        <div className="card-content">
-                            {/* <p>{result.circumstances}</p> */}
-                        </div>     
-                    </div>
+                <Heading className="heading" level={2}>Spotted</Heading>    
+                    <Autocomplete s={12} type="first name" title='First Name' data={{'FirstName': null,}}/>
+                    <Autocomplete s={12} type="last name" title='Last Name' data={{'LastName': null,}} />
+                    <Autocomplete s={12} type="last seen" title='Last Seen' data={{'LastSeen': null,}} />
+                    <Autocomplete s={12} title='State' data={{'State': null,}} />
+                    <Row>
+                    <Input name='on' type='date' onChange={function(e, value) {'Date Spotted'}} />
+                    </Row>
+                    <Row>
+                    <Input type='textarea' title='Notes from Siting'/>
+                    </Row>
+                    <div>
+                    <Button waves='light'>Spotted</Button>
+                    </div>                 
                 </div>
                 </Col>
                 <Col s={12} m={7} className="right-gradient">
                     <div className="right-gradient-content">
-                    <Autocomplete s={12} label='First Name' data={{'FirstName': null,}}/>
-                    <Autocomplete s={12} label='Last Name' data={{'LastName': null,}} />
-                    <Autocomplete s={12} label='Last Name' data={{'LastName': null,}} />
-                    <Autocomplete s={12} label='Last Name' data={{'LastName': null,}} />
-                    </div>
+                    </div>  
                 </Col>
             </Row>
         );
